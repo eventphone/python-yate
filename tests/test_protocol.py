6910188cda90e86f073850f884e28a832ba6af8c
thomasDOTwtf <thomas@drewermann.org>
2018-04-02 12:31:41 +0200

    initial commit

---
+++ tests/test_protocol.py
@@ -0,0 +1,57 @@
+import unittest
+from yate import protocol
+
+
+class EncodingTestCases(unittest.TestCase):
+    def test_encode_bytes(self):
+        result = protocol.yate_encode_bytes(b"test%")
+        self.assertEqual( b"test%%", result)
+
+    def test_encode_bytes2(self):
+        result = protocol.yate_encode_bytes(b"/bin:/usr/bin:/usr/local/bin")
+        self.assertEqual( b"/bin%z/usr/bin%z/usr/local/bin", result)
+
+    def test_decode_bytes(self):
+        result = protocol.yate_decode_bytes(b"test%%")
+        self.assertEqual(b"test%" , result)
+
+    def test_decode_bytes2(self):
+        result = protocol.yate_decode_bytes(b"/bin%z/usr/bin%z/usr/local/bin")
+        self.assertEqual(b"/bin:/usr/bin:/usr/local/bin" , result)
+
+    def test_decode_fails_invalid(self):
+        with self.assertRaises(Exception):
+            result = protocol.yate_decode_bytes(b"/bin%:/usr/bin%:/usr/local/bin")
+
+    def test_parse_yate_msg(self):
+        result = protocol.parse_yatemessage('%%>message:0x7ff823883bb0.1932044751:1522601502:call.execute::id=sip/151:module=sip:status=incoming:address=172.20.23.1%z5060:billid=1522598913-105:answered=false:direction=incoming:callid=sip/4265e70a902406405ac10e1e275@DSIP/f-9024064014ea27935f/:caller=9940 DebügDÄCT:called=2049:callername=DebügDÄCT:antiloop=19:ip_host=172.20.23.1:ip_port=5060:ip_transport=UDP:connection_id=dect:connection_reliable=false:sip_uri=sip%z2049@172.20.23.2:sip_from=sip%z9940@172.20.23.2:sip_to=<sip%z2049@172.20.23.2>:sip_callid=4265e70a902406405ac10e1e275@DSIP:device=Mitel SIP-DECT (SW-Version=7.1-CK14):sip_contact="DebügDÄCT" <sip%z9940@172.20.1.3>;+sip.instance="<urn%zuuid%z1F102AF1-2C00-0100-8000-03029649cf19>":sip_supported=replaces, 100rel, path, gruu:sip_user-agent=Mitel SIP-DECT (SW-Version=7.1-CK14):sip_allow=INVITE, ACK, OPTIONS, CANCEL, BYE, REFER, NOTIFY, INFO, MESSAGE, UPDATE, PRACK:sip_content-type=application/sdp:username=9940:realm=voip.eventphone.de:newcall=true:domain=172.20.23.2:xsip_nonce_age=0:rtp_addr=172.20.23.12:media=yes:formats=alaw,mulaw:transport=RTP/AVP:rtp_rfc2833=101:rtp_port=16326:sdp_silenceSupp=off - - - -:sdp_sendrecv=:sdp_fmtp%z=0-15:rtp_forward=yes:handlers=javascript%z15,regexroute%z40,regexroute%z100,javascript%z15,cdrbuild%z50,regexroute%z80,regexroute%z100,sip%z100,register%z120:context=default:oconnection_id=dect:osip_X-EventphoneID=f45a42b9-35cc-11e8-b1ae-000c2991f54c:osip_X-CallType=default:callto=sip/sip%z2049@172.20.1.3'.encode("utf-8"))
+        self.assertEqual('0x7ff823883bb0.1932044751', result._id)
+        self.assertEqual('1522601502', result._time)
+        self.assertEqual('call.execute', result._name)
+        self.assertEqual('DebügDÄCT', result._params['callername'])
+
+    def test_parse_yate_msg_without_kv(self):
+        result = protocol.parse_yatemessage('%%>message:0x7ff823883bb0.1932044751:1522601502:call.execute:'.encode("utf-8"))
+        self.assertEqual('0x7ff823883bb0.1932044751', result._id)
+        self.assertEqual('1522601502', result._time)
+        self.assertEqual('call.execute', result._name)
+        self.assertEqual({}, result._params)
+
+    def test_encode_yate_install_mgs(self):
+        message = protocol.InstallToYate(1, "call.execute")
+        result = message.encode()
+        self.assertEqual(b'%%>install:1:call.execute', result)
+
+    def test_encode_yate_install_mgs_filter(self):
+        message = protocol.InstallToYate(1, "call.execute", "test1", "test2")
+        result = message.encode()
+        self.assertEqual(b'%%>install:1:call.execute:test1:test2', result)
+
+    def test_encode_yate_msg(self):
+        result = protocol.parse_yatemessage('%%>message:0x7ff823883bb0.1932044751:1522601502:call.execute::id=sip/151:module=sip:status=incoming:address=172.20.23.1%z5060:billid=1522598913-105:answered=false:direction=incoming:callid=sip/4265e70a902406405ac10e1e275@DSIP/f-9024064014ea27935f/:caller=9940 DebügDÄCT:called=2049:callername=DebügDÄCT:antiloop=19:ip_host=172.20.23.1:ip_port=5060:ip_transport=UDP:connection_id=dect:connection_reliable=false:sip_uri=sip%z2049@172.20.23.2:sip_from=sip%z9940@172.20.23.2:sip_to=<sip%z2049@172.20.23.2>:sip_callid=4265e70a902406405ac10e1e275@DSIP:device=Mitel SIP-DECT (SW-Version=7.1-CK14):sip_contact="DebügDÄCT" <sip%z9940@172.20.1.3>;+sip.instance="<urn%zuuid%z1F102AF1-2C00-0100-8000-03029649cf19>":sip_supported=replaces, 100rel, path, gruu:sip_user-agent=Mitel SIP-DECT (SW-Version=7.1-CK14):sip_allow=INVITE, ACK, OPTIONS, CANCEL, BYE, REFER, NOTIFY, INFO, MESSAGE, UPDATE, PRACK:sip_content-type=application/sdp:username=9940:realm=voip.eventphone.de:newcall=true:domain=172.20.23.2:xsip_nonce_age=0:rtp_addr=172.20.23.12:media=yes:formats=alaw,mulaw:transport=RTP/AVP:rtp_rfc2833=101:rtp_port=16326:sdp_silenceSupp=off - - - -:sdp_sendrecv=:sdp_fmtp%z=0-15:rtp_forward=yes:handlers=javascript%z15,regexroute%z40,regexroute%z100,javascript%z15,cdrbuild%z50,regexroute%z80,regexroute%z100,sip%z100,register%z120:context=default:oconnection_id=dect:osip_X-EventphoneID=f45a42b9-35cc-11e8-b1ae-000c2991f54c:osip_X-CallType=default:callto=sip/sip%z2049@172.20.1.3'.encode("utf-8"))
+        result = result.encode_for_yate(False)
+        #self.assertEqual(''.encode("utf-8"),result)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ yate/protocol.py
@@ -0,0 +1,128 @@
+
+
+def yate_decode_bytes(byte_input):
+    output = b""
+    escaped = False
+    for b in byte_input:
+        if escaped:
+            if b == ord("%"):
+                output += bytes([b])
+            else:
+                if b < 64:
+                    raise Exception("Unable to decode yate shitty bytes")
+                output += bytes([(b-64)])
+            escaped = False
+        else:
+            if b == ord("%"):
+                escaped = True
+            else:
+                output += bytes([b])
+    return output
+
+
+''' Encode a sequence of bytes to be sent to yate
+
+Args:
+    byte_input (str): sequence of bytes to be encoded
+
+'''
+def yate_encode_bytes(byte_input):
+    output = b""
+    for b in byte_input:
+        if b<32 or b == ord(":"):
+            output += b"%"+bytes([(b+64)])
+        elif b == ord("%"):
+            output += b"%%"
+        else:
+            output += bytes([b])
+    return output
+
+def yate_decode_split(bytes_input):
+    output = bytes_input.split(b":")
+    output = [yate_decode_bytes(param).decode("utf-8") for param in output]
+    return output
+
+def yate_encode_join(*args):
+    output = [yate_encode_bytes(param.encode("utf-8")) for param in args]
+    return b":".join(output)
+
+def yate_parse_keyvalue(params):
+    output = {}
+    for param in params:
+        key, value = param.split("=",1)
+        output[key] = value
+    return output
+
+
+def parse_yatemessage(bytes_input):
+    splitted = yate_decode_split(bytes_input)
+    messagetype = splitted[0]
+    if messagetype == "%>message":
+        id = splitted[1]
+        time = splitted[2]
+        name = splitted[3]
+        returnvalue = splitted[4]
+        params = yate_parse_keyvalue(splitted[5:])
+        message = MessageFromYate(id, time, name, returnvalue, params)
+        return message
+        pass
+    elif messagetype == "%>install":
+        priority = splitted[1]
+        name = splitted[2]
+        success = True if splitted[3] == "true" else False
+        message = InstallFromYate(priority, name, success)
+        return message
+        pass
+    elif messagetype == "%>uninstall":
+        pass
+    elif messagetype == "%>watch":
+        name = splitted[1]
+        success = splitted[2]
+        message = WatchFromYate(name, success)
+        return message
+        pass
+    elif messagetype == "%>unwatch":
+        pass
+    elif messagetype == "%>setlocal":
+        pass
+
+class MessageFromYate:
+
+    def __init__(self, id, time, name, returnvalue, params):
+        self._id = id
+        self._time = time
+        self._name = name
+        self._returnvalue = returnvalue
+        self._params = params
+
+    def encode_for_yate(self, processed):
+        processed = str(processed).lower()
+        return yate_encode_join("%<message", self._id, processed, "", self._returnvalue, *["=".join(item) for item in self._params.items()])
+
+class InstallToYate:
+
+    def __init__(self, prioriy, name, filtername=None, filtervalue=None):
+        self._priority = str(prioriy)
+        self._name = name
+        self._filtername = filtername
+        self._filtervalue = str(filtervalue)
+
+    def encode(self):
+        extraargs = []
+        if self._filtername is not None:
+            extraargs.append(self._filtername)
+            if self._filtervalue is not None:
+                extraargs.append(self._filtervalue)
+        return yate_encode_join("%>install", self._priority, self._name, *extraargs)
+
+class InstallFromYate:
+    def __init__(self, priority, name, success):
+        self._priority = priority
+        self._name  = name
+        self._success = success
+
+class WatchFromYate:
+
+    def __init__(self, name, success):
+        self._name = name
+        self._success = success
