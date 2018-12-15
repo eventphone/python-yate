21bcc383f58d3577934308c148816499042190e2
Martin Lang <Martin.Lang@rwth-aachen.de>
2018-12-15 02:15:42 +0100

    Prepare Open Source Release
    Add README.md and LICENSE for initial open source
    release

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python support for yate-pbx
+
+This package implements library support for the external module protocol used
+by the yate pbx. There is support for the basic wire protocol and higher level
+abstractions using asyncio - including special support for IVRs (interactive voice
+response) systems.
+
+# Examples
+
+Have a look at the examples folder to see how to use the IVR support library.
+
+Moreover, there is a call generator example in the tools folder.
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='python-yate',
     version='0.1',
@@ -7,6 +10,8 @@ setup(
     url='www.eventphone.de',
     license='MIT',
     author='Martin Lang',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email='Martin.Lang@rwth-aachen.de',
     description='An (asyncio enabled) python library for yate IVRs and extmodules',
     install_requires=[
