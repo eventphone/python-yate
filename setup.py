27f8fbde474a5b147653613dff25c1df3d004ee4
Martin Lang <Martin.Lang@rwth-aachen.de>
2022-08-10 21:24:55 +0200

    Release version 0.4.0
    Release new version 0.4.0 with support for
    Python 3.7 to 3.10.
    
    This release also fixes handling of hang-up events
    correctly for IVRs.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(this_dir, "README.md"), "r") as f:
 
 setup(
     name='python-yate',
-    version='0.3.1',
+    version='0.4.0',
     packages=['yate'],
     url='https://github.com/eventphone/python-yate',
     license='MIT',
@@ -19,9 +19,10 @@ setup(
     description='An (asyncio enabled) python library for yate IVRs and extmodules',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
     ],
     install_requires=[
