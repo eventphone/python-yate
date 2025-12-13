fd9ddc7aa409e879c84a20b3a9b496719551d8e8
Martin Lang <Martin.Lang@rwth-aachen.de>
2025-12-13 22:04:37 +0100

    Version 0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(this_dir, "README.md"), "r") as f:
 
 setup(
     name='python-yate',
-    version='0.4.1',
+    version='0.5.0',
     packages=['yate'],
     url='https://github.com/eventphone/python-yate',
     license='MIT',
